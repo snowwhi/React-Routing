4f9bab4d954590611894e0703c0fba5a095f2670
snowwhi <khadijahanif890@gmail.com>
2026-01-27 13:11:10 -0500

    header

--- Routing/src/App.jsx
+++ Routing/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './Header/Header.jsx'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
   <>
-  <h1 className='bg-sky-950'>Testing</h1>
+ <Header/>
   </>
   )
 }
